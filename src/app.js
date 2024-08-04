cf286815b289b6def064baa44d369006a2e1cc1a
leandro-si <leandro.almeida@uvvnet.com.br>
2024-08-03 23:22:56 -0300

    teste - cadastro de usuário

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/guiapics").then(() => {
+    //console.log("Conectado com o bando")
+}).catch((err) => {
+    console.log("Erro ao conectar com o banco: ", err)
+})
 
 app.get('/', (req, res) => {
     return res.status(200).json({})
---
+++ test/user.test.js
@@ -0,0 +1,21 @@
+const app = require('../src/app')
+const supertest = require('supertest');
+const request = supertest(app);
+
+describe("Cadastro de usuário", () => {
+
+    test("Deve cadastrar um usuário com sucesso", () => {
+
+        let time = Date.now();
+        let email = `${time}@gmail.com`;
+        let user = {name: "Leandro", email: email, password: "123456"};
+
+        return request.post("/user/create")
+            .send(user)
+            .then((response) => {
+                expect(response.statusCode).toEqual(200)
+            }).catch(err => {
+                fail(err)
+            });
+    })
+})
